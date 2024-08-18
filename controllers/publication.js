c28031c083df48a82fca3227f8ec97c72d5aa914
LuisEduardoRodriguez <97996472+LuisEduardoRodriguez@users.noreply.github.com>
2024-08-18 08:40:58 -0500

    carpetas

---
+++ controllers/publication.js
@@ -0,0 +1,6 @@
+// Método de prueba
+export const testPublication = (req, res) => {
+    return res.status(200).send({
+      message: "Mensaje enviado desde el controlador publication.js"
+    });
+  }
