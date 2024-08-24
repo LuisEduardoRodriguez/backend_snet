288ed7cad8d6ffabfea3ea3063d297c43b992cf0
LuisEduardoRodriguez <97996472+LuisEduardoRodriguez@users.noreply.github.com>
2024-08-23 22:04:37 -0500

    commit

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 const router = Router();
-import { testUser, register, login } from "../controllers/user.js";
+import { testUser, register, login, profile, listUsers, updateUser} from "../controllers/user.js";
 import { ensureAuth } from "../middlewares/auth.js";
+
 
 // Definir las rutas
 router.get('/test-user',ensureAuth, testUser);
 router.post('/register', register);
 router.post('/login', login);
+router.get('/profile/:id', ensureAuth ,profile);
+router.get('/list/:page?', ensureAuth, listUsers);
+router.put('/update', ensureAuth, updateUser);
 
 // Exportar el Router
 export default router;
