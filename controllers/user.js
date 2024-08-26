60243df4cabad1a9a49b9b7083ac985609c870c9
LuisEduardoRodriguez <97996472+LuisEduardoRodriguez@users.noreply.github.com>
2024-08-25 22:17:54 -0500

    metodo user update

--- controllers/user.js
+++ controllers/user.js
@@ -207,3 +207,75 @@ export const listUsers = async (req, res) => {
     });
   }
 }
+// Método para actualizar los datos del usuario
+export const updateUser = async (req, res) => {
+  try {
+    // Obtener la información del usuario a actualizar
+    let userIdentity = req.user;
+    let userToUpdate = req.body;
+
+    // Eliminar campos que nos sobran (no vamos a actualizar)
+    delete userToUpdate.iat;
+    delete userToUpdate.exp;
+    delete userToUpdate.role;
+
+    // Comprobar si el usuario ya existe
+    const users = await User.find({
+      $or: [
+        { email: userToUpdate.email },
+        { nick: userToUpdate.nick },
+      ]
+    }).exec();
+
+    // Verificar si el usuario está duplicado y evitar conflictos
+    const isDuplicateUser = users.some(user => {
+      return user && user._id.toString() !== userIdentity.userId;
+    });
+
+    if(isDuplicateUser) {
+      return res.status(400).send({
+        status: "error",
+        message: "Error: solo se puede actualizar los datos del usuario logueado"
+      });
+    }
+
+    // Cifrar la contraseña si se proporciona
+    if(userToUpdate.password) {
+      try {
+        let pwd = await bcrypt.hash(userToUpdate.password, 10);
+        userToUpdate.password = pwd;
+      } catch (hashError) {
+        return res.status(500).send({
+          status: "error",
+          message: "Error al cifrar la contraseña"
+        });
+      }
+    } else {
+      delete userToUpdate.password;
+    }
+
+    // Buscar y actualizar
+    let userUpdated = await User.findByIdAndUpdate(userIdentity.userId, userToUpdate, { new: true });
+
+    if(!userUpdated){
+      return res.status(400).send({
+        status: "error",
+        message: "Error al actualizar el usuario"
+      });
+    }
+
+    // Devolver la respuesta exitosa
+    return res.status(200).send({
+      status: "sucess",
+      message: "Usuario actualizado correctamente",
+      user: userUpdated
+    });
+
+  } catch (error) {
+    console.log("Error al actualizar el usuario:", error)
+    return res.status(500).send({
+      status: "error",
+      message: "Error al actualizar el usuario"
+    });
+  }
+}
